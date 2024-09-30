85af8ff320cf0cd296033849d13b9227785a2352
EnxhiHoxhaj <hoxhajenxhi1@gmail.com>
2024-09-30 21:21:46 +0200

    feat: add data to database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             CategoryTableSeeder::class,
+            TagsTableSeeder::class,
             PostsSeeder::class,
         ]);
     }
--- database/seeders/TagsTableSeeder.php
+++ database/seeders/TagsTableSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Tag;
+use App\Functions\Helper;
 
 class TagsTableSeeder extends Seeder
 {
@@ -12,6 +14,12 @@ class TagsTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $data = ['Front End', 'Back End', 'Design', 'UX', 'Laravel', 'VueJs', 'Angular', 'React'];
+        foreach($data as $tag) {
+            $new_tag = new Tag();
+            $new_tag->name = $tag;
+            $new_tag->slug = Helper::generateSlug($new_tag->name, Tag::class);
+            $new_tag->save();
+        }
     }
 }
