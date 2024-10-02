c63f55deddacbb8fb673f4658fa2c1e7ae972c96
EnxhiHoxhaj <hoxhajenxhi1@gmail.com>
2024-10-02 11:45:51 +0200

    feat: add validation

--- app/Http/Controllers/Api/PageController.php
+++ app/Http/Controllers/Api/PageController.php
@@ -4,8 +4,18 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PageController extends Controller
 {
-    //
+    public function index(){
+
+        $posts= Post::orderBy('id', 'desc');
+        $succsess= true;
+        $response= [
+            'succsess'=>$succsess,
+            'results'=>$posts,
+        ];
+        return response()->json($response);
+    }
 }
